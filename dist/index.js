dbe7bfdedcf0870fe0df2bc52800c5d8007d15a0
NissimBet <nissimbet123@outlook.com>
2020-04-04 02:02:15 -0500

    gitignore fixed

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const server_1 = require('./server');
-const config_1 = require('./utils/config');
-server_1.app.listen(config_1.PORT, () =>
-  console.log(`Listening at port ${config_1.PORT}`)
-);
